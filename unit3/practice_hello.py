d2a5306269d85aec355d1f6af3017dceee208ee8
hadesyi <leesk55@gmail.com>
2018-12-28 15:24:59 +0900

    변수와 입력 사용하기

--- unit3/practice_hello.py
+++ unit3/practice_hello.py
@@ -1,2 +1,3 @@
 print('Hello, world!')
 print('Python Programming')
+print(102*0.6+225)
---
+++ unit6/unit6.1.py
@@ -0,0 +1,28 @@
+x = 10
+print(x)
+y = 'Hello, world!'
+print(y)
+print(type(x))
+print(type(y))
+
+x, y, z = 10, 20, 30
+print(x)
+print(y)
+print(z)
+
+x = y = z = 10
+print(x)
+print(y)
+print(z)
+
+x, y = 10, 20
+x, y = y, x
+print(x)
+print(y)
+
+x = 10
+del x
+print(x)
+
+x = None
+print(x)
