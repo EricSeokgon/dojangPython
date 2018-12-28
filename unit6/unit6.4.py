5aadb08edac5f701b91ab1c817f8fbb2788d22d4
hadesyi <leesk55@gmail.com>
2018-12-28 15:54:52 +0900

    map을 사용하여 정수로 변환하기

--- unit6/unit6.4.py
+++ unit6/unit6.4.py
@@ -1,4 +1,11 @@
-#입력 값을 변수 두개에 저장하기
-a,b =input('문자열 두개를 입력하세요: ').split()
+# 입력 값을 변수 두개에 저장하기
+a, b = input('문자열 두개를 입력하세요: ').split()
 print(a)
 print(b)
+a = int(a)
+b = int(b)
+print(a + b)
+print(int(a) + int(b))
+
+a, b = map(int, input('숫자 두 개를 입력하세요: ').split())
+print(a + b)
