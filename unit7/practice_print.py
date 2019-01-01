4b81cef2738f4f5457eb968bd9d99aea2241a29a
hadesyi <leesk55@gmail.com>
2019-01-02 08:47:15 +0900

    연습문제: 날짜와 시간 출력하기

--- unit7/practice_print.py
+++ unit7/practice_print.py
@@ -1,9 +1,10 @@
 year = 2000
-month = 10
-day = 27
-hour = 11
-minute = 43
-second = 59
+month = 12
+day = 31
+hour = 10
+minute = 37
+second = 21
 
-print(year, month, day, sep='/', end=' ')
+
+print(year, month, day, sep='-', end='T')
 print(hour, minute, second, sep=':')
